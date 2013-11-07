0e4fa6e71715da4f14a3282d37c1fa164c3c7c4d
Dale Weber <hybotics.pdx@gmail.com>
2013-11-06 22:13:59 -0800

    Reformatted most long comments into multi-line comments, and added 	blank lines to make code more readable.

--- application.go
+++ application.go
@@ -1,7 +1,8 @@
-// Copyright (c) 2012 Brian Hetro <whee@smaertness.net>
-// Use of this source code is governed by the ISC
-// license that can be found in the LICENSE file.
-
+/*
+	Copyright (c) 2012 Brian Hetro <whee@smaertness.net>
+	Use of this source code is governed by the ISC
+		license that can be found in the LICENSE file.
+*/
 package appnet
 
 import (
@@ -48,7 +49,9 @@ type Response struct {
 
 func (c *Application) request(r *Request, name string, args EpArgs) (body io.ReadCloser, err error) {
 	var path bytes.Buffer
+
 	err = epTemplates.ExecuteTemplate(&path, name, args)
+
 	if err != nil {
 		return
 	}
@@ -56,52 +59,69 @@ func (c *Application) request(r *Request, name string, args EpArgs) (body io.Rea
 	ep := ApiEndpoints[name]
 	url := path.String()
 	req, err := http.NewRequest(string(ep.Method), url, r.Body)
+
 	if err != nil {
 		return
 	}
 
 	req.Header.Set("X-ADN-Migration-Overrides", "response_envelope=1")
+
 	if r.Token != "" {
 		req.Header.Set("Authorization", "Bearer "+r.Token)
 	}
+
 	if r.BodyType != "" {
 		req.Header.Set("Content-Type", r.BodyType)
 	}
 
 	resp, err := apiHttpClient.Do(req)
+
 	if err != nil {
 		return
 	}
+
 	body = resp.Body
 
 	return
 }
 
-// Do handles all API requests.
-// The Request contains the authentication token and optional body.
-// The name comes from ApiEndpoints, with template arguments provided in args.
-// The response is unpacked into v.
-//
-// In the future, you would not call this function directly, instead using a helper
-// function for the specific action.
-func (c *Application) Do(r *Request, name string, args EpArgs, v interface{}) error {
+/*
+	Do handles all API requests.
+
+	The Request contains the authentication token and optional body.
+
+	The name comes from ApiEndpoints, with template arguments
+		provided in args.
+
+	The response is unpacked into v.
+
+	In the future, you would not call this function directly, but
+		instead use this helper function for the specific action.
+*/
+func (c *Application) Do (r *Request, name string, args EpArgs, v interface{}) (err error) {
 	body, err := c.request(r, name, args)
+
 	if err != nil {
-		return err
+		return
 	}
+
 	defer body.Close()
 
 	resp, err := ioutil.ReadAll(body)
+
 	if err != nil {
-		return err
+		return
 	}
 
 	epOptions := ApiEndpoints[name].Options
+
 	if epOptions == nil || epOptions.ResponseEnvelope {
 		re := &responseEnvelope{Data: v}
+
 		err = json.Unmarshal(resp, re)
+
 		if err != nil {
-			return err
+			return
 		}
 
 		if re.Meta.ErrorId != "" {
@@ -109,32 +129,41 @@ func (c *Application) Do(r *Request, name string, args EpArgs, v interface{}) er
 		}
 	} else {
 		err = json.Unmarshal(resp, v)
+
 		if err != nil {
-			return err
+			return
 		}
 	}
 
-	return err
+	return
 }
 
 // Generate the authentication URL for the server-side flow.
-func (c *Application) AuthenticationURL(state string) (string, error) {
+func (c *Application) AuthenticationURL (state string) (string, error) {
 	var url bytes.Buffer
+
 	args := struct {
 		*Application
 		State string
 	}{c, state}
+
 	err := epTemplates.ExecuteTemplate(&url, "authentication url", args)
+
 	if err != nil {
 		return "", err
 	}
+
 	return url.String(), nil
 }
 
-// During server-side flow, the user will be redirected back with a code.
-// AccessToken uses this code to request an access token for the user.
-// This token is returned as a string.
-func (c *Application) AccessToken(code string) (string, error) {
+/*
+	During server-side flow, the user will be redirected back with a
+		code.
+
+	AccessToken() uses this code to request an access token for the
+		user, which is returned as a string.
+*/
+func (c *Application) AccessToken (code string) (string, error) {
 	data := url.Values{}
 	data.Set("client_id", c.Id)
 	data.Set("client_secret", c.Secret)
@@ -151,19 +180,25 @@ func (c *Application) AccessToken(code string) (string, error) {
 		AccessToken string `json:"access_token"`
 		Error       string
 	}{}
+
 	err := c.Do(r, "get access token", EpArgs{}, resp)
+
 	if err != nil {
 		return "", err
 	}
+
 	if resp.Error != "" {
 		return "", errors.New(resp.Error)
 	}
+
 	return resp.AccessToken, nil
 }
 
-// PasswordToken is used to carry out the password flow. The function submits
-// the username and password to get an access token. This token is returned
-// as a string.
+/*
+	PasswordToken is used to carry out the password flow. The function
+		submits the username and password to get an access token. This
+		token is returned as a string.
+*/
 func (c *Application) PasswordToken (userName, userPassword string) (aToken string, err error) {
 	data := url.Values{}
 	data.Set("client_id", c.Id)
